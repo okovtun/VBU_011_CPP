904666375e742309c02611fa8c4b52209762bfe1
Clayman <Clayman@DESKTOP-DM8E77U>
2021-06-20 17:22:40 +0300

    Assignment operator.

--- Intro/Point/main.cpp
+++ Intro/Point/main.cpp
@@ -5,6 +5,7 @@ using std::cout;
 using std::endl;
 
 #define tab "\t"
+#define delimiter "\n------------------------------------------------------------\n"
 
 class Point
 {
@@ -27,6 +28,66 @@ public:
 	{
 		this->y = y;
 	}
+
+	//			Constructors:
+	Point()
+	{
+		//Конструктор по умолчанию
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		//Конструктор с одним параметром
+		this->x = x;
+		this->y = 0;
+		cout << "SingleArgumentConstructor:\t" << this << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	Point(const Point& other)
+	{
+		//Shallow copy - повехностное копирование объекта
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//			Operators:
+	Point& operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+
+	//			Methods:
+
+	double distance(const Point& other)
+	{
+		//A - this;
+		//B - other;
+		//other.x += 100;
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		//sqrt - Square Root (Квадратный корень).
+		return distance;
+	}
+
+	void print()const
+	{
+		cout << "X = " << x << tab << "Y = " << y << endl;
+	}
 };
 //Создавая структуру или класс мы создаем тип данных.
 //Классы и структуры еще называют пользовательскими типами данных.
@@ -34,7 +95,18 @@ public:
 //					СТРУКТУРА - ЭТО ТИП ДАННЫХ!!!
 //						КЛАСС - ЭТО ТИП ДАННЫХ!!!
 
+double distance(const Point& A, const Point& B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance*x_distance + y_distance * y_distance);
+	return distance;
+}
+
 //#define STRUCT_POINT
+#define DISTANCE_CHECK
+//#define CONSTRUCTORS_CHECK
+//#define ASSIGNMENT_CHECK
 
 void main()
 {
@@ -57,16 +129,62 @@ void main()
 	cout << (*pA).x << tab << (*pA).y << endl;
 #endif // STRUCT_POINT
 
+#ifdef DISTANCE_CHECK
 	Point A;
 	A.set_x(2);
 	A.set_y(3);
 	cout << A.get_x() << tab << A.get_y() << endl;
+	Point B;
+	B.set_x(4);
+	B.set_y(5);
+	cout << delimiter << endl;
+	cout << B.distance(A) << endl;	//Мы находимся в точке B, и вычисляем расстояние от этой точки (B) до точки A.
+	cout << delimiter << endl;
+	cout << A.distance(B) << endl;
+	cout << delimiter << endl;
+	cout << distance(A, B) << endl;
+	cout << delimiter << endl;
+	cout << distance(B, A) << endl;
+	cout << delimiter << endl;
 
-	for (int i = 0; i < 5; i++)
+	/*for (int i = 0; i < 5; i++)
 	{
 		cout << i << tab;
 	}
-	cout << endl;
+	cout << endl;*/
+#endif // DISTANCE_CHECK
+
+#ifdef CONSTRUCTORS_CHECK
+	Point A;	//Default constructor
+	A.print();
+	Point B = 3;//Single-argument constructor
+	B.print();
+	Point C(2, 3);//Parametrized constructor
+	C.print();
+	//ДЕСТРУКТОР ВЫЗЫВАЕТСЯ НЕ ДЛЯ КОНСТРУКТОРА, ДЕСТРУКТОР ВЫЗЫВАЕТСЯ ДЛЯ ОБЪЕКТА
+	//Shallow copy
+	Point D = C;	//CopyConstructor
+	D.print();
+	Point E;	//DefaultConstructor
+	E = C;		//CopyAssignment
+	E.print();
+#endif // CONSTRUCTORS_CHECK
+
+#ifdef ASSIGNMENT_CHECK
+	
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << tab << b << tab << c << endl;
+
+	Point A, B, C;
+	cout << "\n-----------------------------------------\n";
+	A = B = C = Point(4, 5);
+	cout << "\n-----------------------------------------\n";
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGNMENT_CHECK
+
 }
 
 /*
