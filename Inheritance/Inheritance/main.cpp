03338619dd96d27fa817186a7240e4cdac3de42d
Clayman <Clayman@DESKTOP-DM8E77U>
2021-08-01 14:22:06 +0300

    Inheritance int Academy DONE.

---
+++ Inheritance/Inheritance/main.cpp
@@ -0,0 +1,222 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+#define delimiter "\n----------------------------------------------\n"
+
+class Human
+{
+	string last_name;
+	string first_name;
+	unsigned int age;
+public:
+	const string& get_last_name()const
+	{
+		return last_name;
+	}
+	const string& get_firt_name()const
+	{
+		return first_name;
+	}
+	unsigned int get_age()const
+	{
+		return age;
+	}
+	void set_last_name(const string& last_name)
+	{
+		this->last_name = last_name;
+	}
+	void set_first_name(const string& first_name)
+	{
+		this->first_name = first_name;
+	}
+	void set_age(unsigned int age)
+	{
+		this->age = age;
+	}
+
+	//				Constructors
+	Human(const string& last_name, const string& first_name, unsigned int age)
+	{
+		set_last_name(last_name);
+		set_first_name(first_name);
+		set_age(age);
+		cout << "HConstructor:\t" << this << endl;
+	}
+	~Human()
+	{
+		cout << "HDestructor:\t" << this << endl;
+	}
+
+	//				Methods:
+	void print()const
+	{
+		cout << last_name << " " << first_name << ", " << age;
+		print_year_suffix(age);
+		cout << endl;
+	}
+protected:
+	void print_year_suffix(unsigned int age)const
+	{
+		if (age%20 >= 5 && age%20 <= 20)cout << " лет";
+		else if (age % 10 >= 5 || age % 10 == 0)cout << " лет";
+		else if (age % 10 > 1 && age % 10 < 5) cout << " года";
+		else cout << " год";
+	}
+};
+
+class Student : public Human
+{
+	string speciality;
+	string group;
+	double rating;
+public:
+	const string& get_speciality()const
+	{
+		return speciality;
+	}
+	const string& get_group()const
+	{
+		return group;
+	}
+	double get_rating()const
+	{
+		return rating;
+	}
+	void set_speciality(const string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_group(const string& group)
+	{
+		this->group = group;
+	}
+	void set_rating(double rating)
+	{
+		this->rating = rating;
+	}
+
+	//				Constructors
+	Student
+	(
+		const string& last_name, const string& first_name, unsigned int age,
+		const string& speciality, const string& group, double rating
+	) :Human(last_name, first_name, age)
+	{
+		set_speciality(speciality);
+		set_group(group);
+		set_rating(rating);
+		cout << "SCostructor:\t" << this << endl;
+	}
+	~Student()
+	{
+		cout << "SDestructor:\t" << this << endl;
+	}
+
+	//				Methods:
+	void print()const
+	{
+		Human::print();
+		cout << "Специальность: " << speciality << ", группа: " << group << ", успеваемость: " << rating << endl;
+	}
+};
+
+class Teacher :public Human
+{
+	string speciality;
+	unsigned int experience;
+public:
+	const string& get_speciality()const
+	{
+		return speciality;
+	}
+	unsigned int get_experience()const
+	{
+		return experience;
+	}
+	void set_speciality(const string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_experience(unsigned int experience)
+	{
+		this->experience = experience;
+	}
+	//			Constructors:
+	Teacher
+	(
+		const string& last_name, const string& first_name, unsigned int age,
+		const string& speciality, unsigned int experience
+	) :Human(last_name, first_name, age)
+	{
+		set_speciality(speciality);
+		set_experience(experience);
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Teacher()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+
+	//			Methos:
+	void print()const
+	{
+		Human::print();
+		cout << "Специальность: " << speciality << ", опыт преподавания: " << experience;
+		print_year_suffix(experience);
+		cout << endl;
+	}
+};
+
+class Graduate :public Student
+{
+	string diploma_theme;
+public:
+	const string& get_diploma_theme()const
+	{
+		return diploma_theme;
+	}
+	void set_diploma_theme(const string& diploma_theme)
+	{
+		this->diploma_theme = diploma_theme;
+	}
+	Graduate
+	(
+		const string& last_name, const string& first_name, unsigned int age,
+		const string& speciality, const string& group, double rating,
+		const string& diploma_theme
+	) :Student(last_name, first_name, age, speciality, group, rating)
+	{
+		set_diploma_theme(diploma_theme);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	~Graduate()
+	{
+		cout << "GDestructor:\t" << this << endl;
+	}
+
+	//			Methods:
+	void print()const
+	{
+		Student::print();
+		cout << "Тема дипломного проекта: " << diploma_theme << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Наследование" << endl;
+	Human vasya("Тупенко", "Василий", 22);
+	vasya.print();
+	cout << delimiter << endl;
+	Student student("Иванов", "Федор", 25, "РПО", "ВБУ_011", 90);
+	student.print();
+	cout << delimiter << endl;
+	Teacher	teacher("Einstein", "Albert", 142, "Astronomy", 112);
+	teacher.print();
+
+	cout << delimiter << endl;
+	Graduate graduate("Musk", "Elon", 34, "Car building", "VBU_011", 98, "Колонизация марса");
+	graduate.print();
+}
--- Intro/HomeWork/main.cpp
+++ Intro/HomeWork/main.cpp
@@ -1,387 +1,130 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-//#define constructors_check
-//#define increment_check
-//#define arichmetical_operators_check
-//#define TYPE_CONVERSIONS_HOME_WORK
+class Matrix {
+	int dim1;
+	int dim2;
+	int** arr;
 
-class Fraction;
-Fraction operator*(Fraction left, Fraction right);
-bool operator==(Fraction left, Fraction rigth);
-bool operator>(Fraction left, Fraction rigth);
-
-int  euclideanGcd(int a, int b);
-
-class Fraction
-{
-
-	int integer;  //целая часть
-	int numerator; //Числитель
-	int denominator;// Знаменатель
 public:
-	int get_integer() const
-	{
-		return integer;
+	Matrix() {
+		dim1 = 0;
+		dim2 = 0;
+		arr = nullptr;
 	}
-
-	int get_numerator() const
-	{
-		return numerator;
-	}
-
-	int get_denominator() const
-	{
-		return denominator;
-	}
-	void set_integer(int integer)
-	{
-		this->integer = integer;
-	}
-	void set_numerator(int numerator)
-	{
-		this->numerator = numerator;
-	}
-	void set_denominator(int denominator)
-	{
-		if (denominator == 0) denominator = 1;
-		this->denominator = denominator;
-	}
-
-	// Constructors:
-	Fraction()
-	{
-		this->integer = 0;
-		this->numerator = 0;
-		this->denominator = 1;
-		cout << "DefaultConstructor:\t" << this << endl;
-	}
-	explicit Fraction(int integer)
-	{
-		this->integer = integer;
-		this->numerator = 0;
-		this->denominator = 1;
-		cout << "1argConstructor:\t" << this << endl;
-	}
-
-	Fraction(int numerator, int denominator)
-	{
-		this->integer = 0;
-		this->numerator = numerator;
-		this->set_denominator(denominator);// можем просто set_denominator(denominator)
-		cout << "Constructor:\t" << this << endl;
-	}
-
-
-	Fraction(int integer, int numerator, int denominator)
-	{
-		this->integer = integer;
-		this->numerator = numerator;
-		this->set_denominator(denominator);
-		cout << "Constructor:\t" << this << endl;
-	}
-
-	Fraction(const Fraction& other)
-	{
-		this->integer = other.integer;
-		this->numerator = other.numerator;
-		this->denominator = other.denominator;
-		cout << "CopyConstructor:" << this << endl;
-
-	}
-
-	~Fraction()
-	{
-		cout << "Destructor:\t" << this << endl;
-	}
-
-	// Operators:
-	Fraction& operator=(const Fraction& other)
-	{
-		this->integer = other.integer;
-		this->numerator = other.numerator;
-		this->denominator = other.denominator;
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
-	}
-
-	Fraction& operator*=(const Fraction& other)
-	{
-		//*this = *this * other;
-		return *this = *this * other; ;
-	}
-
-
-	// Increment/Decrement:
-
-	Fraction& operator++()
-	{
-		integer++;
-		return *this;
-	}
-	Fraction operator++(int) //Postfix increment
-	{
-		Fraction old = *this;//сохраняем старое значениею Copy Constructor
-		integer++;
-		return old;// CopyConstructor
-	}
-
-	Fraction& operator--()
-	{
-		integer--;
-		return *this;
-	}
-
-	// type cast operator
-	explicit operator int()
-	{
-		return integer;
-	}
-
-	//        Methods
-	Fraction& to_improper()
-	{
-		numerator += integer * denominator;
-		integer = 0;
-		return *this;
-	}
-	Fraction& to_proper()
-	{
-		integer += numerator / denominator;
-		numerator %= denominator;
-		return *this;
-	}
-
-	Fraction& reduce()
-	{
-		to_proper();
-		int max_com_div = euclideanGcd(numerator, denominator);
-		numerator /= max_com_div;
-		denominator /= max_com_div;
-		return *this;
-
-	}
-
-	void print()const
-	{
-		if (integer) cout << integer;
-		if (integer && numerator) cout << "(";
-		if (numerator) cout << numerator << "/" << denominator;
-		if (integer && numerator) cout << ")";
-		if (integer == 0 && numerator == 0) cout << 0;
+	Matrix(int dim1, int dim2) {
+		this->dim1 = dim1;
+		this->dim2 = dim2;
+		arr = new int*[dim1];
+		for (int i = 0; i < dim1; i++) {
+			arr[i] = new int[dim2] {};
+		}
+		for (int i = 0; i < dim1; i++) {
+			for (int j = 0; j < dim2; j++) {
+				cout << "\tplease,enter arr[" << i << "]" << "[" << j << "]" << endl;
+				cin >> arr[i][j];
+			}
+		}
 		cout << endl;
-
 	}
-	Fraction inverted()// обращает дробь
-	{
-		to_improper();
-		/*int buffer = numerator;
-		numerator = denominator;
-		denominator = buffer;
-		return *this;*/
-		return Fraction(denominator, numerator);
+
+	Matrix(const Matrix& other) {
+		this->dim1 = other.dim1;
+		this->dim2 = other.dim2;
+		this->arr = new int*[dim1];
+		for (int i = 0; i < dim1; i++) {
+			this->arr[i] = new int[dim2];
+		}
+		for (int i = 0; i < dim1; i++) {
+			for (int j = 0; j < dim2; j++) {
+				this->arr[i][j] = other.arr[i][j];
+			}
+		}
+	}
+
+	Matrix& operator=(const Matrix& other) {
+
+		this->dim1 = other.dim1;
+		this->dim2 = other.dim2;
+		this->arr = new int*[dim1];
+		for (int i = 0; i < dim1; i++) {
+			this->arr[i] = new int[dim2];
+		}
+		for (int i = 0; i < dim1; i++) {
+			for (int j = 0; j < dim2; j++) {
+				this->arr[i][j] = other.arr[i][j];
+			}
+		}
+		return *this;
+	}
+	void set_arr() {
+		arr = new int*[dim1];
+		for (int i = 0; i < dim1; i++) {
+			arr[i] = new int[dim2] {};
+		}
+	}
+	void set_ij(int i, int j, int value) {
+		arr[i][j] = value;
+	}
+	void set_dim1(int a) {
+		dim1 = a;
+	}
+	void set_dim2(int a) {
+		dim2 = a;
+	}
+	int get_dim1() {
+		return dim1;
+	}
+	int get_dim2() {
+		return dim2;
+	}
+	int get_ij(int i, int j) {
+		return arr[i][j];
+	}
+	void print() {
+		for (int i = 0; i < dim1; i++) {
+			for (int j = 0; j < dim1; j++) {
+				cout << arr[i][j] << "\t";
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	void _delete() {
+		for (int i = 0; i < dim1; i++) {
+			delete[] arr[i];
+		}
+		delete[] arr;
 	}
 };
+Matrix operator+(Matrix& left, Matrix& right) {
+	Matrix temp;
+	if (left.get_dim1() == right.get_dim1() && left.get_dim2() == right.get_dim2()) {
 
-Fraction operator* (Fraction left, Fraction rigth)
-{
-	left.to_improper();
-	rigth.to_improper();
-	/*Fraction result;
-	result.set_numerator(left.get_numerator() * rigth.get_numerator());
-	result.set_denominator(left.get_denominator() * rigth.get_denominator());*/
-
-
-	/*Fraction result
-	(
-		left.get_numerator() * rigth.get_numerator(),
-		left.get_denominator() * rigth.get_denominator()
-	);
-		return result;*/
-	return Fraction
-	(
-		left.get_numerator() * rigth.get_numerator(),
-		left.get_denominator() * rigth.get_denominator()
-	);
-
-}
-Fraction operator/(Fraction left, Fraction rigth)
-{
-	//left.to_improper();
-	//rigth.to_improper();
-	//return Fraction
-	//(
-	//	left.get_numerator() * rigth.get_denominator(),
-	//	left.get_denominator() * rigth.get_numerator()
-	//);
-
-	return left * rigth.inverted();
-
-}
-Fraction operator+(Fraction left, Fraction rigth)
-{
-	left.to_improper();
-	rigth.to_improper();
-	//Fraction sum(
-	//	left.get_numerator()* rigth.get_denominator() +
-	//	rigth.get_numerator()* left.get_denominator(),
-	//	left.get_denominator() * rigth.get_denominator()
-	//);
-	//sum.to_proper();
-	//return sum;
-	return Fraction(left.get_numerator()* rigth.get_denominator() +
-		rigth.get_numerator()* left.get_denominator(),
-		left.get_denominator() * rigth.get_denominator()
-	).to_proper();
-}
-
-
-
-bool operator==(Fraction left, Fraction rigth)
-{
-	left.to_proper();
-	rigth.to_proper();
-	left.reduce();
-	rigth.reduce();
-
-
-	return
-		(left.get_integer() == rigth.get_integer() &&
-			left.get_numerator() == rigth.get_numerator() &&
-			left.get_denominator() == rigth.get_denominator());
-}
-
-bool operator!=(Fraction left, Fraction rigth)
-{
-	return !(left == rigth);
-
-}
-bool operator>(Fraction left, Fraction rigth)
-{
-	left.to_improper();
-	rigth.to_improper();
-
-
-	return  (double)left.get_numerator() / left.get_denominator() > (double)rigth.get_numerator() / rigth.get_denominator();
-	//return (double)left > (double)right;
-
-}
-bool operator>=(const Fraction& left, const Fraction& right)
-{
-	return  (left > right) || (left == right);
-}
-
-
-int  euclideanGcd(int a, int b) {
-	int t;
-	while (b != 0)
-	{
-
-		t = b;
-		b = a % b;
-		a = t;
+		temp.set_dim1(left.get_dim1());
+		temp.set_dim2(left.get_dim2());
+		temp.set_arr();
+		for (int i = 0; i < temp.get_dim1(); i++) {
+			for (int j = 0; j < temp.get_dim2(); j++) {
+				temp.set_ij(i, j, left.get_ij(i, j) + right.get_ij(i, j));
+			}
+		}
 
 	}
-	return a;
-
-
+	return temp;
 }
-
-void main()
-
-{
-
-#ifdef constructors_check
-
-	Fraction A; //Default consructor
+int main() {
+	Matrix A(2, 2);
 	A.print();
-	Fraction B = 3; //Default consructor
+	Matrix B = A;
 	B.print();
-	Fraction C(3, 4);
+	Matrix C;
+	C = A;
 	C.print();
-	Fraction D(2, 3, 4);
+	Matrix D;
+	D = A + B;
 	D.print();
-	Fraction E = D; //CopyConstructor
-	E.print();
 
-	Fraction F;
-	F = E; // Copy Assignment
-	F.print();
-
-#endif // constructors_check
-#ifdef arichmetical_operators_check
-	Fraction A(2, 3, 4);
-	Fraction B(3, 4, 5);
-	Fraction C = A * B;
-	C.print();
-	Fraction D = A / B;
-	D.print();
-	Fraction E = A + B;
-	E.print();
-#endif 
-
-#ifdef increment_check
-
-	for (double i = .25; i < 10; i++)
-	{
-		cout << i << "\t";
-	}
-	cout << endl;
-#endif
-	//A.print();
-	//B.print();
-	//A.to_improper();
-	//A.print();
-	//A.to_proper();
-	//A.print();
-
-	//Fraction A(2, 3, 4);
-	//Fraction B(3, 4, 5); 
-	//A *=  B;// implicit operator call
-	//A.operator*=(B); // explicit operator call
-	//operator*(A,B); // explicit operator call
-	//A.print();
-
-
-	//int a = 2;
-	//Fraction A = Fraction(5);
-	//A.print();
-	//Fraction B;
-	//B =(Fraction)3;
-	//B.print();
-
-	//int b;
-	//b = (int)B;
-
-	/*int a = 3;
-	int b = 6;
-	cout << euclideanGcd(6, 3);*/
-	Fraction A = Fraction(2, 6, 12);
-	A.print();
-	A.reduce();
-	A.print();
-	Fraction B = Fraction(2, 7, 12);
-	B.print();
-	B.reduce();
-	B.print();
-	cout << endl << "(A == B): " << (A == B) << endl;
-	cout << endl << "(A != B): " << (A != B) << endl;
-	cout << endl << "(A > B): " << (A > B) << endl;
-	/*operator type()
-	{
-		..........
-		..........
-	}*/
-#ifdef TYPE_CONVERSIONS_HOME_WORK
-	//Task1
-	Fraction A(2, 3, 4);
-	double a = A;
-	cout << a << endl;
-
-	//Task2
-	double b = 3.14;
-	Fraction B = b;
-	B.print();
-#endif // TYPE_CONVERSIONS_HOME_WORK
-
+	return 0;
 }
