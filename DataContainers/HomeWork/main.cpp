d0a2e31682432214ecec33aa6c2a3065e99eb71e
Clayman <Clayman@DESKTOP-DM8E77U>
2021-10-03 12:49:05 +0300

    Recursion DONE.

---
+++ DataContainers/HomeWork/main.cpp
@@ -0,0 +1,89 @@
+﻿//Recursion, Factorial, Power
+#include<iostream>
+using namespace std;
+#define FACTORIAL_RECURSION
+//#define POWER_RECURSION
+
+#ifdef POWER_RECURSION
+double power = 1;
+void Power(int stepen, float m)
+{
+	if (stepen == 0)return;
+	//cout << stepen << endl;
+	power = power * m;
+	Power(stepen - 1, m); // рекурсивный вызов
+}
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a;
+	float m;
+	int n;
+	cout << "Введите основание: "; cin >> m;
+	cout << "Введите степень числа: "; cin >> a;
+	int p = a;
+	float c = a - p;
+	n = a * a;
+	n = sqrt(n);
+	Power(n, m);
+
+	//cout << c << endl;
+	//cout << p << endl;
+	cout << endl;
+	if (c != 0)
+	{
+		cout << "Я не настолько продвинутый, чтоб возводить" << endl;
+		cout << "в дробную степень. Она будет округлена до: " << p << endl;
+	}
+	if (a < 0)
+	{
+		cout << "Результат: " << 1 / power << endl;
+	}
+	else
+	{
+		cout << "Результат: " << power << endl;
+	}
+
+}
+#endif // POWER_RECURSION
+
+#ifdef FACTORIAL_RECURSION
+unsigned long long Factorial = 1;
+void elevator(int level)
+{
+	if (level == 0) return;
+	if (level > 60) return;
+	if (level < 0) return;
+	//cout << level << endl;
+	Factorial = Factorial * level;
+	elevator(level - 1); // рекурсивный вызов
+}
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Факториал: ";
+	int n;
+	cout << "Введите число: "; cin >> n;
+	elevator(n);
+	cout << endl;
+	if (n > 60)
+	{
+		Factorial = 0;
+		cout << "Много, столько не надо знать." << endl;
+		cout << "Машину хочешь повесить??? - Не выйдет!" << endl;
+	}
+	else
+	{
+		if (n < 0)
+		{
+			//cout << "Факториал: --" << endl;
+			cout << "Не бывает факториала от отрицательных чисчел! Учи матчасть!" << endl;
+		}
+		else
+		{
+			cout << "Факториал: " << Factorial << endl;
+		}
+	}
+	cout << endl;
+}
+#endif // FACTORIAL_RECURSION
--- DataContainers/Recursion/main.cpp
+++ DataContainers/Recursion/main.cpp
@@ -1,6 +1,7 @@
 //Recursion
 #include<iostream>
 using namespace std;
+
 void elevator(int level)
 {
 	if (level == 0)return;
@@ -8,14 +9,50 @@ void elevator(int level)
 	elevator(level - 1);//Рекурсивный вызов
 	cout << level << endl;
 }
+
+int factorial(int n)
+{
+	/*if (n == 0)return 1;
+	return n * factorial(n - 1);*/
+	return n == 0 ? 1 : n * factorial(n - 1);
+}
+
+double power(double a, int n)
+{
+	/*if (n == 0)return 1;
+	if (n > 0)return a * power(a, n - 1);
+	else return 1 / a * power(a, n + 1);*/
+	return n == 0 ? 1 : n > 0 ? a * power(a, n - 1) : 1 / a * power(a, n + 1);
+}
+
+//#define RECURSION_BASICS
+//#define FACTORIAL
+#define POWER
+
 void main()
 {
 	setlocale(LC_ALL, "Russian");
+#ifdef RECURSION_BASICS
 	//cout << "Hello World";
-	//main();	//Рекурсивный вызов функции main()
+//main();	//Рекурсивный вызов функции main()
 	int n;
 	cout << "Введите номер этажа: "; cin >> n;
 	elevator(n);
+#endif // RECURSION_BASICS
+
+#ifdef FACTORIAL
+	int n;
+	cout << "Введите число для вычисления факториала: "; cin >> n;
+	cout << factorial(n) << endl;
+#endif // FACTORIAL
+
+	int a;	//Основание степени
+	int n;	//Показател степени
+	cout << "Введите основание степени: "; cin >> a;
+	cout << "Введите показатель степени: "; cin >> n;
+	cout << power(a, n) << endl;
+
+	main();
 }
 
 //Итерационно - при помощи циклов
